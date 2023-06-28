f9553e8a5075df2f380136c93577dc3665ca2198
mosunsetMyPC <driveasano@gmail.com>
2023-06-28 12:03:59 +0900

    スライドショーのオフライン化

--- js/index.js
+++ js/index.js
@@ -14,20 +14,26 @@ document.addEventListener('DOMContentLoaded', (event) => {
 });
 
 function imgload() {
-    var req = new XMLHttpRequest();
-    req.open("get", "./img/homeslide/images-number.csv", true);
-    req.send(null);
-    req.onload = function () {
-
-        var intervaltime = 3500;
-        for (var i = 0; i < req.responseText; i++) {
-            if (i == 0) {
-                $('.carousel-indicators').append(`<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1" title="1"></button>`);
-                $('.carousel-inner').append(`<div class="carousel-item active" data-bs-interval="${intervaltime}"><img src="./img/homeslide/0.jpg" onerror="this.src='./img/no-img.jpg'" class="d-block w-100" alt="slide写真0"></div>`);
-            } else {
-                $('.carousel-indicators').append(`<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="${i}" aria-label="Slide ${i + 1}" title="${i + 1}"></button>`);
-                $('.carousel-inner').append(`<div class="carousel-item" data-bs-interval="${intervaltime}"><img src="./img/homeslide/${i}.jpg" onerror="this.src='./img/no-img.jpg'" class="d-block w-100" alt="slide写真${i}"></div>`);
-            }
+    var intervaltime = 3500;
+    for (var i = 0; i < 10; i++) { // 0～9までの10枚
+        if (i == 0) { // 最初の選択画像また必ず1枚は画像があるものとする
+            $('.carousel-indicators').append(`<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1" title="1"></button>`);
+            $('.carousel-inner').append(`<div class="carousel-item active" data-bs-interval="${intervaltime}"><img src="./img/homeslide/0.jpg" class="d-block w-100" alt="slide写真 0"></div>`);
+        } else {
+            loadImage(`./img/homeslide/${i}.jpg`).then(res => {
+                var x = Number(res.src.substr(-5).substr(0, 1));
+                $('.carousel-indicators').append(`<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="${x}" aria-label="Slide ${x + 1}" title="${x + 1}"></button>`);
+                $('.carousel-inner').append(`<div class="carousel-item" data-bs-interval="${intervaltime}"><img src="${res.src}" class="d-block w-100" alt="slide写真 ${x}"></div>`);
+            });
         }
     }
 }
+
+function loadImage(src) {
+    return new Promise((resolve) => {
+        const img = new Image();
+        img.onload = () => resolve(img);
+        img.src = src;
+    });
+}
+
